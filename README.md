941379086c35c8d69381f01f8668eeba729ec744
Rose Pezzuti Dyer <pezzutidyer@uchicago.edu>
2025-07-09 11:47:44 -0500

    make on PDF embedding in chromadb

---
+++ README.md
@@ -0,0 +1,17 @@
+```
+uv venv .env
+source .env/bin/activate
+```
+
+```
+uv pip install whatever-package
+```
+
+
+# Resources
+- https://medium.com/@danushidk507/rag-with-llama-using-ollama-a-deep-dive-into-retrieval-augmented-generation-c58b9a1cfcd3
+- https://dev.to/mohsin_rashid_13537f11a91/rag-with-ollama-1049
+- https://weaviate.io/blog/local-rag-with-ollama-and-weaviate
+- https://ollama.com/blog/embedding-models
+- https://medium.com/@vndee.huynh/build-your-own-rag-and-run-it-locally-langchain-ollama-streamlit-181d42805895
+- https://cookbook.chromadb.dev/core/document-ids/#semantic-strategies
--- make-one-embedding.py
+++ make-one-embedding.py
@@ -1,20 +1,48 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 import sys
+import chromadb
+from chromadbx import UUIDGenerator
 
-# Load the document
-if len(sys.argv) > 1:
-    path = sys.argv[1]
-else:
-    print("No path provided, exiting.")
-    exit(1)
+# To call:
+# python3 make-one-embedding.py "/Users/pezzutidyer/Documents/AbqBackyardRefuge/" "my_doc_id" "18-014.pdf" "abq_backyard_refuge"
 
-print(f"Loading document from: {path}")
-loader = PyPDFLoader(path)
-documents = loader.load()
+document_dir = sys.argv[1]
+document_id = sys.argv[2]
+file_name = sys.argv[3]
+chromadb_name = sys.argv[4]
+file_path = document_dir + file_name
+chromadb_path = document_dir + chromadb_name
+
+
+chroma_client = chromadb.PersistentClient(path=chromadb_path)
+collection = chroma_client.create_collection(name=chromadb_name)
+
+print(f"Loading document from: {file_path}")
+loader = PyPDFLoader(file_path)
+document = loader.load()
 
 # Split the document into chunks
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
-docs = text_splitter.split_documents(documents=documents)
+document_chunks = text_splitter.split_documents(documents=document)
 
-print(docs)
+for index,chunk in enumerate(document_chunks):
+    chunk_id = f"{document_id}_{index}" 
+    print(f"Processing index {index}")
+    print(f"Chunk ID: {chunk_id}")
+    print(f"Chunk length: {len(chunk.page_content)}")
+    print(f"Chunk metadata: {chunk.metadata}")
+    print(f"Chunk content: {chunk.page_content[:100]}...")  # Print first 100 characters of the chunk
+    print("-" * 80)  # Separator for readability
+
+    # collection.upsert(
+    # documents=[
+    #     "This is a document about pineapple",
+    #     "This is a document about oranges"
+    # ],
+    # ids=["id1", "id2"]
+# )
+# TODO make ids for document chunks
+# see https://cookbook.chromadb.dev/core/document-ids/#semantic-strategies
+
+    collection.add( ids=[chunk_id], documents=[ chunk.page_content])
